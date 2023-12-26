3c266112cabdd8b0bb1bf2782127c5102e29fae4
Nikolai <kolya.kurenkov.1999@gmail.com>
2023-12-27 01:44:06 +0300

    Fix

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 from array import array
 
-def Mars() -> None:
+def mars_roverars(robot, limmits) -> int:
     """Марсоход."""
-    robots: array[int] = [int(x) for x in input().split(' ')] 
-    limit = int(input())
     pos_max: int = 0 #Позиция самого большого числа в массиве.
-    pos_min: int = len(robots) - 1 #Позиция самого минимального числа в массиве.
-    robots.sort(reverse=True) 
-    while robots[pos_max] > limit or pos_max == pos_min + 1:
-        #Перенос позиции самого большого в рамки предела
-        pos_max += 1
+    pos_min: int = len(robot) - 1 #Позиция самого минимального числа в массиве.
+    robot.sort(reverse=True) 
     count: int = 0
-    while pos_max != pos_min and pos_min > pos_max:
-        if (robots[pos_max] + robots[pos_min]) <= limit:
+    while pos_max <= pos_min and pos_min > pos_max:
+        if (robot[pos_max] + robot[pos_min]) <= limmits:
             pos_min -= 1
         pos_max += 1
         count += 1
     if pos_min == pos_max:       
         count += 1         
-    print(count)
+    return count
 
+robots: array = [int(x) for x in input().split(' ')]
+limit = int(input())
+print(mars_roverars(robots, limit))
 if __name__ == '__main__':
-    Mars()
+    mars_roverars(robots, limit)
